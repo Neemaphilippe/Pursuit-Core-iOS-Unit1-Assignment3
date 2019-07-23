a0aa6b29a4c9aaaf989610f1df2f61facf5989ed
Neema Philippe <neemaphilippe@neemas-mbp.home>
2019-07-22 23:38:42 -0400

    Answered Calc Project

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -8,17 +8,67 @@
 
 import Foundation
 
-func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
-  switch opString {
-  case "+":
-    return {x, y in x + y }
-  case "-":
-    return {x, y in x - y }
-  case "*":
-    return {x, y in x * y }
-  case "/":
-    return {x, y in x / y }
-  default:
-    return {x, y in x + y }
-  }
+var userOperation = readLine()!
+let number = readLine()
+let number2 = readLine()
+var numInput = Int()
+//part1a
+enum BasicMath: String  {
+    case addition = "+"
+    case subtraction = "-"
+    case multiplication =  "*"
+    case division = "/"
+    
+    func math(x: String?) {
+        let c = BasicMath.init(rawValue: userOperation)
+        switch self {
+        case .addition:
+            if let response1 = number,
+                let response2 = number2,
+                let num1 = Int(response1),
+                let num2 = Int(response2) {
+                
+                var answer = num1 + num2
+                print("\(num1) + \(num2) = \(answer)")
+                
+            }
+            
+        case .subtraction:
+            if let response1 = number,
+                let response2 = number2,
+                let num1 = Int(response1),
+                let num2 = Int(response2) {
+                
+                var answer = num1 - num2
+                print("\(num1) - \(num2) = \(answer)")
+                
+            }
+        case .multiplication:
+            if let response1 = number,
+                let response2 = number2,
+                let num1 = Int(response1),
+                let num2 = Int(response2) {
+                
+                var answer = num1 * num2
+                print("\(num1) * \(num2) = \(answer)")
+                
+            }
+        case .division:
+            if let response1 = number,
+                let response2 = number2,
+                let num1 = Int(response1),
+                let num2 = Int(response2) {
+                
+                var answer = num1 / num2
+                print("\(num1) / \(num2) = \(answer)")
+                
+            }
+        }
+    }
 }
+
+var currentOperation = BasicMath(rawValue: userOperation)
+currentOperation!.math(x:userOperation)
+
+
+
